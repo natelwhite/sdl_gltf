d8b6aeeca5f5c258cb7b19922a323a8dcaac978e
Nate White <natelwhite01@gmail.com>
2025-04-22 11:49:51 -0400

    Include world position as vertex shader output

--- build/shaders/source/PositionTransform.vert.hlsl
+++ build/shaders/source/PositionTransform.vert.hlsl
@@ -13,12 +13,14 @@ struct Output
 {
     float4 Position : SV_Position;
     float3 Normal : NORMAL;
+	float4 WorldPos : TEXCOORD1;
 };
 
 Output main(Input input)
 {
     Output output;
-    output.Position = mul(mul(proj_view, model), float4(input.Position, 1.0f));
+	output.Position = mul(mul(proj_view, model), float4(input.Position, 1.0f));
     output.Normal = normalize(mul((float3x3)model, input.Normal));
+	output.WorldPos = mul(model, float4(input.Position, 1.0f));
     return output;
 }
